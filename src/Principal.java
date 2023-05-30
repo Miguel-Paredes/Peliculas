3fdb2cc2e05586a1be9688552aa0e5764eea2892
Miguel-Paredes <os.miguelparedes@gmail.com>
2023-05-30 09:27:43 -0500

    prueba

--- src/Principal.java
+++ src/Principal.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class principal {
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
 }
--- src/TiposPeliculas.java
+++ src/TiposPeliculas.java
@@ -1,2 +1,4 @@
-package PACKAGE_NAME;public class TiposPeliculas {
+public class TiposPeliculas {
+    //las instancias son los objetos
+
 }
