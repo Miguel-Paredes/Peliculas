14dc8e3375f69f853f98600cb07531c5532c4b4a
Cristhian Panora <cristhian.panora@gmail.com>
2023-06-01 09:09:41 -0500

    4 peliculas

--- src/Principal.java
+++ src/Principal.java
@@ -2,11 +2,50 @@ import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("Hola");
+        System.out.println("Datos de peliculas");
+        //instancias
+        Scanner entrada = new Scanner(System.in);
+
+
+        TiposPeliculas HarryPoter = new TiposPeliculas(
+                "Harry Potter y el cáliz de fuego",
+                "Fantasía", 2005,
+                "Estados Unidos",
+                "Daniel Radcliffe",
+                97,
+                "PG-13",
+                "Reino Unido",
+                "Heyday Films");
+        TiposPeliculas Megan = new TiposPeliculas(
+                "Megan",
+                "Terror",
+                2023,
+                "Estados Unidos",
+                "Amie Donald",
+                102,
+                "PG-13",
+                "Estados Unidos",
+                "Universal Pictures");
+        TiposPeliculas Encanto = new TiposPeliculas(
+                "Encanto",
+                "infantil",
+                2021,
+                "Walt Disney Picture",
+                "Stefanie Beatriz",
+                109,
+                "Todo publico",
+                "Estados Unidos",
+                "Walt Disney Studios");
+        TiposPeliculas Eljusticiero = new TiposPeliculas(
+                "El Justiciero 2",
+                "Accion",
+                2018,
+                "Estados Unidos",
+                "Denzel Washington",
+                121,
+                "violenta",
+                "Estados Unidos",
+                "Columbia Pictures");
+        HarryPoter.imprimirpelicula();
     }
-
-    //instancias
-    Scanner entrada = new Scanner(System.in);
-    TiposPeliculas terror = new ;
-
 }
--- src/TiposPeliculas.java
+++ src/TiposPeliculas.java
@@ -1,6 +1,114 @@
 public class TiposPeliculas {
-    //las instancias son los objetos
-    //prueba de ejercicio
-    //me llamo Aldahir
-    //te llamas davo
+    //atributos
+    String NomPelicula;
+    String tipo;
+    int year;
+    String lugar_creacion;
+    String act_principal;
+    int duracion;
+    String clasificacion;
+    String pais;
+    String company_creacion;
+
+    //constructor
+    public TiposPeliculas(String NomPelicula, String tipo, int year, String lugar_creacion, String act_principal, int duracion, String clasificacion, String pais, String company_creacion) {
+        this.NomPelicula = NomPelicula;
+        this.tipo = tipo;
+        this.year = year;
+        this.lugar_creacion = lugar_creacion;
+        this.act_principal = act_principal;
+        this.duracion = duracion;
+        this.clasificacion = clasificacion;
+        this.pais = pais;
+        this.company_creacion = company_creacion;
+    }
+
+    //métodos
+
+    public String getNomPelicula() {
+        return NomPelicula;
+    }
+
+    public void setNomPelicula(String nomPelicula) {
+        NomPelicula = nomPelicula;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getLugar_creacion() {
+        return lugar_creacion;
+    }
+
+    public void setLugar_creacion(String lugar_creacion) {
+        this.lugar_creacion = lugar_creacion;
+    }
+
+    public String getAct_principal() {
+        return act_principal;
+    }
+
+    public void setAct_principal(String act_principal) {
+        this.act_principal = act_principal;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getClasificacion() {
+        return clasificacion;
+    }
+
+    public void setClasificacion(String clasificacion) {
+        this.clasificacion = clasificacion;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getCompany_creacion() {
+        return company_creacion;
+    }
+
+    public void setCompany_creacion(String company_creacion) {
+        this.company_creacion = company_creacion;
+    }
+    public void inprimirpelicula(){
+        System.out.println(this.NomPelicula);
+        System.out.println(this.act_principal);
+        System.out.println(this.tipo);
+        System.out.println(this.year);
+        System.out.println(this.duracion + " min");
+        System.out.println(this.pais);
+        System.out.println(this.clasificacion);
+        System.out.println(this.company_creacion);
+        System.out.println(this.lugar_creacion);
+    }
+
+
+}
+
 }
